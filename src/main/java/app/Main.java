cf1a3ba3d46c106108e59445911e5b9931620d2b
Emil Kriegel <54576311+emil2112@users.noreply.github.com>
2025-05-07 12:17:00 +0200

    SendGrid logic and Jackson dependencies

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,9 +5,17 @@ import app.config.ThymeleafConfig;
 import app.controllers.OrderController;
 import app.controllers.UserController;
 import app.persistence.ConnectionPool;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 
@@ -51,6 +59,46 @@ public class Main {
             orderController.OrderCreate(ctx);
 
         });
+        Email from = new Email("emilkriegel@gmail.com");
+        from.setName("Johannes Fog Byggemarked");
+
+        Mail mail = new Mail();
+        mail.setFrom(from);
+
+        String API_KEY = System.getenv("SENDGRID_API_KEY");
+
+        Personalization personalization = new Personalization();
+
+        /* Erstat kunde@gmail.com, name, email og zip med egne værdier ****/
+        /* I test-fasen - brug din egen email, så du kan modtage beskeden */
+        personalization.addTo(new Email("mbjerrumj@gmail.com"));
+        personalization.addDynamicTemplateData("name", "Anders And");
+        personalization.addDynamicTemplateData("login", "anders@and.dk");
+        personalization.addDynamicTemplateData("number", "12345678");
+        mail.addPersonalization(personalization);
+
+        mail.addCategory("carportapp");
+
+        SendGrid sg = new SendGrid(API_KEY);
+        Request request = new Request();
+        try
+        {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+
+            // indsæt dit skabelonid herunder
+            mail.templateId = "d-1b6aefc418c3427880a7df567316899d";
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Error sending mail");
+            ex.printStackTrace();
+        }
     }
 
 }
